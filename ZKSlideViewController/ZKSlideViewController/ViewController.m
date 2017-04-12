d2c12da918fad98e6fbc1404b4ecf0cf5f4881e3
ZK <dev.zhoukang@gmail.com>
2017-04-12 11:22:10 +0800

    sync

--- ZKSlideViewController/ZKSlideViewController/ViewController.m
+++ ZKSlideViewController/ZKSlideViewController/ViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "ZKHotViewController.h"
+#import "ZKListViewController.h"
+#import "ZKVideoViewController.h"
+#import "ZKHeadViewController.h"
+#import "ZKNewsViewController.h"
+#import "ZKProfileViewController.h"
 
 @interface ViewController ()
 
@@ -16,7 +22,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setupChildViewControllers];
+}
+
+- (void)setupChildViewControllers {
+    ZKHotViewController *hotVC = [ZKHotViewController new];
+    hotVC.title = @"热门";
+    [self addChildViewController:hotVC];
+    
+    ZKListViewController *listVC = [ZKListViewController new];
+    listVC.title = @"榜单";
+    [self addChildViewController:listVC];
+    
+    ZKVideoViewController *videoVC = [ZKVideoViewController new];
+    videoVC.title = @"视频";
+    [self addChildViewController:videoVC];
+    
+    ZKHeadViewController *headVC = [ZKHeadViewController new];
+    headVC.title = @"头条";
+    [self addChildViewController:headVC];
+    
+    ZKNewsViewController *newsVC = [ZKNewsViewController new];
+    newsVC.title = @"新闻";
+    [self addChildViewController:newsVC];
+    
+    ZKProfileViewController *profielVC = [ZKProfileViewController new];
+    profielVC.title = @"个人";
+    [self addChildViewController:profielVC];
 }
 
 @end
--- ZKSlideViewController/ZKSlideViewController/ZKSlideViewController/ZKSlideViewController.m
+++ ZKSlideViewController/ZKSlideViewController/ZKSlideViewController/ZKSlideViewController.m
@@ -8,12 +8,6 @@
 
 #import "ZKSlideViewController.h"
 #import "ZKSlideHeader.h"
-#import "ZKHotViewController.h"
-#import "ZKListViewController.h"
-#import "ZKVideoViewController.h"
-#import "ZKHeadViewController.h"
-#import "ZKNewsViewController.h"
-#import "ZKProfileViewController.h"
 
 @interface ZKSlideViewController () <UIScrollViewDelegate>
 
@@ -45,11 +39,18 @@ static const CGFloat kTitleScrollViewHeight = 50.f;
     
     [self setupTitleScrollView];
     [self setupContentScrollView];
-    [self setupChildViewControllers];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
     [self setupTitles];
 }
 
 - (void)setupTitles {
+    if (_titleBtns.count) {
+        return;
+    }
+    
     NSInteger count = self.childViewControllers.count;
     CGFloat btnWidth = 100.f;
     CGFloat btnHeight = CGRectGetHeight(_titleScrollView.frame);
@@ -129,32 +130,6 @@ static const CGFloat kTitleScrollViewHeight = 50.f;
     
     CGFloat x = SCREEN_WIDTH * index;
     _contentScrollView.contentOffset = CGPointMake(x, 0);
-}
-
-- (void)setupChildViewControllers {
-    ZKHotViewController *hotVC = [ZKHotViewController new];
-    hotVC.title = @"热门";
-    [self addChildViewController:hotVC];
-    
-    ZKListViewController *listVC = [ZKListViewController new];
-    listVC.title = @"榜单";
-    [self addChildViewController:listVC];
-    
-    ZKVideoViewController *videoVC = [ZKVideoViewController new];
-    videoVC.title = @"视频";
-    [self addChildViewController:videoVC];
-    
-    ZKHeadViewController *headVC = [ZKHeadViewController new];
-    headVC.title = @"头条";
-    [self addChildViewController:headVC];
-    
-    ZKNewsViewController *newsVC = [ZKNewsViewController new];
-    newsVC.title = @"新闻";
-    [self addChildViewController:newsVC];
-    
-    ZKProfileViewController *profielVC = [ZKProfileViewController new];
-    profielVC.title = @"个人";
-    [self addChildViewController:profielVC];
 }
 
 - (void)setupContentScrollView {
